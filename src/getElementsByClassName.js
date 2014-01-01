35ddd03e527acbd5e9b780dba84b5de62405a284
jgpettibone <jeanette.pettibone@gmail.com>
2014-01-01 10:30:27 -0800

    getElements... doesn't pass one spec

--- src/getElementsByClassName.js
+++ src/getElementsByClassName.js
@@ -3,7 +3,34 @@
 //   return document.getElementsByClassName(className);
 // };
 
-// But in stead we're going to implement it from scratch:
+// But instead we're going to implement it from scratch:
 var getElementsByClassName = function (className) {
-  // your code here
+
+    var arr = []; 
+
+    if ( typeof arguments[1] === 'undefined' ) { 
+        var elements = document.body; 
+    } else {
+	var elements = arguments[1];
+    }
+
+    if (elements.className === className) { 
+        arr.push(elements); 
+    }
+
+    var children = elements.childNodes;  
+
+    if ( typeof children != 'undefined') {
+	var childLen = children.length;
+	if ( childLen != 0 )  {
+	    for (var i=0; i<childLen; i++) {
+ 	        var newArr = getElementsByClassName(className, children[i]);
+	        if (typeof newArr != 'undefined') {
+ 	            arr = arr.concat(newArr);
+	        }
+            }
+        } 
+    }
+    return arr;
+
 };
